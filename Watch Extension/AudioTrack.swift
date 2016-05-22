ed6602fbb8cfd626e314084700757b880cfb007b
Cotard <masha.patani@gmail.com>
2016-05-23 02:19:40 +0300

    Fixed bug with wrong url to audios

--- Watch Extension/AudioTrack.swift
+++ Watch Extension/AudioTrack.swift
@@ -12,26 +12,29 @@ class AudioTrack {
     
     let title: String
     let artist: String
+    let url: NSURL
     
-    class func audiosFromFilesPaths(files: [String]) -> [AudioTrack] {
+    
+    class func audiosFromFilesUrls(files: [NSURL]) -> [AudioTrack] {
         var audios = [AudioTrack]()
         
         for file in files {
-            let audioAsset = WKAudioFileAsset(URL: NSURL(fileURLWithPath: file))
+            let audioAsset = WKAudioFileAsset(URL: file)
             
             let title = (audioAsset.title != nil) ? audioAsset.title! : "Unknown Track"
             let artist = (audioAsset.artist != nil) ? audioAsset.artist! : "Unknown Artist"
             
-            let audioTrack = AudioTrack(title: title, artist: artist)
+            let audioTrack = AudioTrack(title: title, artist: artist, url: file)
             audios.append(audioTrack)
         }
         
         return audios
     }
     
-    init(title: String, artist: String) {
+    init(title: String, artist: String, url: NSURL) {
         self.title = title
         self.artist = artist
+        self.url = url
     }
     
 }
--- Watch Extension/AudiosInterfaceController.swift
+++ Watch Extension/AudiosInterfaceController.swift
@@ -13,6 +13,7 @@ import Foundation
 class AudiosInterfaceController: WKInterfaceController {
 
     let kRowType = "AudioRow"
+    var audioTracks = [AudioTrack]()
     
     @IBOutlet var audiosTable: WKInterfaceTable!
     
@@ -21,7 +22,7 @@ class AudiosInterfaceController: WKInterfaceController {
         
         // Configure interface objects here.
         let files = FileManager.audioFiles()
-        let audioTracks = AudioTrack.audiosFromFilesPaths(files)
+        audioTracks = AudioTrack.audiosFromFilesUrls(files)
         
         audiosTable.setNumberOfRows(audioTracks.count, withRowType: kRowType)
         
@@ -40,6 +41,13 @@ class AudiosInterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        let audio = audioTracks[rowIndex]
+        presentMediaPlayerControllerWithURL(audio.url, options: [WKMediaPlayerControllerOptionsAutoplayKey: true]) { (didPlayToEnd: Bool, endTime: NSTimeInterval, error: NSError?) in
+            //
+        }
     }
 
 }
