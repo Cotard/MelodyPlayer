2f83d65639c26ccd8349c8970c7423495e93076e
Cotard <masha.patani@gmail.com>
2016-05-22 23:37:15 +0300

    Added AudioTrack

---
+++ Watch Extension/AudioTrack.swift
@@ -0,0 +1,21 @@
+//
+//  AudioTrack.swift
+//  MelodyPlayer
+//
+//  Created by user on 22.05.16.
+//  Copyright © 2016 Patani. All rights reserved.
+//
+
+import WatchKit
+
+class AudioTrack {
+    
+    let title: String
+    let duration: String
+    
+    init(title: String, duration: String) {
+        self.title = title
+        self.duration = duration
+    }
+    
+}
